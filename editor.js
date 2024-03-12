272f02a9f7b8cf064f2fdb3b71729945b2e7432f
hukl <contact@smyck.org>
2024-03-12 08:52:53 +0100

    working editor poc

--- editor.js
+++ editor.js
@@ -29,54 +29,59 @@ function Checklist() {
 
 
   function parse_md(checklist_markup) {
-    let
-      lines       = checklist_markup.split("\n");
-    console.log(lines);
-
+    let lines       = checklist_markup.split("\n");
 
     let fold_fun = function(acc, line) {
       let match_result = null;
 
       if ((match_result = line.match(/^#\s(.+)$/))) {
-        acc.push("Titel: " + match_result[1]);
+        acc.push(new_checklist(match_result[1]));
       } else if ((match_result = line.match(/^(\w+):\s(.+)$/))) {
-        acc.push("Action: " + match_result[1] + ": " + match_result[2]);
+        let current_checklist_element = acc.slice(-1)[0];
+        let current_item_list         = current_checklist_element.querySelector("ul");
+        let new_item                  = new_checklist_item(match_result[1], match_result[2]);
+
+        current_item_list.appendChild(new_item);
       }
 
       return acc;
     };
 
-    //let result = fold(fold_fun, [], lines);
     let result = lines.reduce(fold_fun, []);
-    console.log(result);
+
+    let new_checklist_container = generate_element(
+      "<div id='checklist_container'></div>"
+    );
+
+    result.forEach(function(c) {new_checklist_container.appendChild(c);});
+    console.log("hi", new_checklist_container);
+    document.querySelector("div#checklist_container").replaceWith(new_checklist_container);
   }
 
+  function new_checklist(title) {
+    let checklist_element = generate_element(
+      "<div class='checklist'><h1>" + title + "</h1><ul></ul></div>"
+    );
 
-  //while (lines.length > 0) {
-  //  //
-  //  var n = lines.shift();
-  //  if ("# " === n.substring(0, 2))
-  //    accumulator.push({
-  //      title: n.substring(2),
-  //      checklists: []
-  //    });
-  //  else if ("## " === n.substring(0, 3))
-  //    accumulator.slice(-1)[0]["checklists"].push({
-  //      title: n.substring(3),
-  //      items: []
-  //    });
-  //  else if ("* " === n.substring(0, 2)) {
-  //    var s = n.indexOf(": ");
-  //    s >= 0 ? accumulator.slice(-1)[0]["checklists"].slice(-1)[0].items.push({
-  //      subject: n.substring(2, s),
-  //      operation: n.substring(s + 2)
-  //    }) : accumulator.slice(-1)[0]["checklists"].slice(-1)[0].items.push({
-  //      subject: n.substring(2)
-  //    });
-  //  }
-  //}
-  //checklist_set = accumulator;
-  //}
+    return checklist_element;
+  }
+
+  function new_checklist_item(item_name, action) {
+    let checklist_item = generate_element(
+      "<li><div class='checklist_item'>" +
+        item_name + "<span class='dots'></span>" + action +
+      "</div></li>"
+    );
+
+    return checklist_item;
+  }
+
+  // Expects html with exactly one top level element
+  function generate_element(html) {
+    const template = document.createElement("template");
+    template.innerHTML = html.trim();
+    return template.content.children[0];
+  }
 
   return {
     init
